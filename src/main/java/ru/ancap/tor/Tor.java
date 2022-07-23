02052d563ad7cf304995aac23109d926e9b84784
PukPukov <90708073+PukPukov@users.noreply.github.com>
2022-07-23 10:26:51 +0500

    Cleaned up

---
+++ src/main/java/ru/ancap/tor/Tor.java
@@ -0,0 +1,17 @@
+package ru.ancap.tor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
